7ce2e3ed303f77f16e4ec440e4f113fd21759854
ronishzadode <ronishzadode@gmail.com>
2016-10-01 16:35:09 +0530

    Formatted output

--- owm1.py
+++ owm1.py
@@ -1,3 +1,8 @@
 import os,sys,time
-t=time.strftime("%Y-%m-%d")
-os.system("python owm.py | grep "+t+" -A 2 -B 14")
+d=time.strftime("%Y-%m-%d")
+t=time.strftime("%H:%M:%S")	
+os.system("python owm.py | grep "+d+" -A 2 -B 14 > output.txt")
+os.system("sed -i 's/u//g' output.txt")
+f=open("output.txt","r")
+f1=f.read()
+print f1
