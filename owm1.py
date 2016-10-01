e8a7695970f313945492d94c721c928eab0a6c11
ronishzadode <ronishzadode@gmail.com>
2016-10-01 18:26:52 +0530

    Time for which you want the results

--- owm1.py
+++ owm1.py
@@ -1,13 +1,22 @@
 import os,sys,time
 d=time.strftime("%Y-%m-%d")
-t=time.strftime("%H:%M:%S")	
-os.system("python owm.py | grep "+d+" -A 2 -B 14 > output.txt")
-os.system("sed -i 's/u//g' output.txt")
-os.system("sed -i 's/poplation/population/g' output.txt")
-os.system("sed -i 's/hmidity/humidity/g' output.txt")
-os.system("sed -i 's/clods/clouds/g' output.txt")
-os.system("sed -i 's/grnd/ground/g' output.txt")
-os.system("sed -i 's/contry/country/g' output.txt")
-f=open("output.txt","r")
-f1=f.read()
-print f1
+def getWeather():	
+	os.system("python owm.py | grep "+d+" -A 14 -B 2 > output.txt")
+	os.system("sed -i 's/u//g' output.txt")
+	os.system("sed -i 's/poplation/population/g' output.txt")
+	os.system("sed -i 's/hmidity/humidity/g' output.txt")
+	os.system("sed -i 's/clods/clouds/g' output.txt")
+	os.system("sed -i 's/grnd/ground/g' output.txt")
+	os.system("sed -i 's/contry/country/g' output.txt")
+	
+getWeather()	
+n=input("For what time do you want to see today's weather?\n1. 12pm\n2. 3pm\n3. 6pm\n4. 10pm\n")	
+if(n==1):	
+	os.system("cat output.txt | grep 12:00:00 -A 14 -B 2")
+elif(n==2):
+	os.system("cat output.txt | grep 15:00:00 -A 14 -B 2")
+elif(n==3):
+	os.system("cat output.txt | grep 18:00:00 -A 14 -B 2")
+elif(n==4):
+	os.system("cat output.txt | grep 21:00:00 -A 14 -B 2")
+		
